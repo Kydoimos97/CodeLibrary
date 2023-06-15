913d8f4fb02a230c4d235d25ba56f053d172c41a
Kydoimos97 <willemvanderschans97@gmail.com>
2023-06-15 14:35:32 -0600

    Build 0.0.2

--- setup.py
+++ setup.py
@@ -7,19 +7,4 @@ DESCRIPTION = 'Reusable Code Library'
 setup(
     name="CodeLib",
     version=VERSION,
-    author="Willem van der Schans",
-    author_email="willemvanderschans97+CodeLib@gmail.com",
-    description=DESCRIPTION,
-    long_description=DESCRIPTION,
-    packages=find_packages(),
-    install_requires=[],  # additional packages
-
-    keywords=['python', 'code library'],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Reference",
-        "Programming Language :: Python :: 3",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
-)
+    packages=find_packages())
