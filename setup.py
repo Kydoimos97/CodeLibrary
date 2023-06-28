1de080aaab81b4935d8cb30b980d34c53a9e76a8
Kydoimos97 <willemvanderschans97@gmail.com>
2023-06-28 14:06:21 -0600

    Source Update

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pandas~=1.5.3", "requests~=2.29.0"]
+
 
 setup(
-    name='Codelib',
-    version='1.0',
-    packages=['Reference_CodeLibrary'],
-    url='https://github.com/Kydoimos97/CodeLibrary',
-    license='GPL-3.0',
-    author='Willem van der Schans',
-    author_email='willemvanderschans97+codelib@gmail.com',
-    description='In-house code library to promote code re-usability'
+    name="K5CodeLibrary",
+    version="0.0.1",
+    author="Willem van der Schans",
+    author_email="willemvanderschans97+codelib@gmail.com",
+    description="In-house code library to promote code re-usability",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='https://www.github.com/Kydoimos97/CodeLibrary',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
 )
